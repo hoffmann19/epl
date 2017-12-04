3c12c769c6130521fac3fe0b52ffd3c70af69651
Charliechaf <cchafin@labelinsight.com>
2017-12-03 21:47:34 -0600

    This will show a scatter plot that you can select the variables for. If you pick a column name with a space in it you will get an error.

--- Shiny.R
+++ Shiny.R
@@ -1,13 +1,23 @@
 library(shiny)
-
+install.packages('DT')
+library(DT)
+library(ggplot2)
 #teamchoices = sort(unlist(as.list(unique(combineddb$team))))
+columnlist = sort(unlist(as.list(colnames(Averages[,c(2:length(colnames(Averages)))]))))
 
 ui <- fluidPage(
-  dataTableOutput('mytable')
+ selectInput('x','Choose your X axis',choices = columnlist),
+ selectInput('y','Choose your Y axis',choices = columnlist),
+ dataTableOutput('mytable'),
+ plotOutput('scatter')
 )
 
 server = function(input, output) {
-  output$mytable = renderDataTable({combineddb})
+  dataset <- reactive({
+    totals[,c('team',input$x,input$y)]
+  })
+  p = reactive({ggplot(dataset(),aes_string(x=input$x, y=input$y))+geom_point()})
+  output$scatter = renderPlot(p())
 }
 
 shinyApp(ui = ui, server = server)
