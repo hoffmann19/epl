a254fe8c3d8a61b9a6b29c592947aa8e932cfde2
Charliechaf <cchafin@labelinsight.com>
2017-12-03 22:31:14 -0600

    Set the default chart as goals/passes. Updated the scatter plot to look a little prettier. Added a totals table to the scraper

--- Shiny.R
+++ Shiny.R
@@ -6,8 +6,8 @@ library(ggplot2)
 columnlist = sort(unlist(as.list(colnames(Averages[,c(2:length(colnames(Averages)))]))))
 
 ui <- fluidPage(
- selectInput('x','Choose your X axis',choices = columnlist),
- selectInput('y','Choose your Y axis',choices = columnlist),
+ selectInput('x','Choose your X axis',choices = columnlist,selected = 'Goals'),
+ selectInput('y','Choose your Y axis',choices = columnlist, selected = 'Passes'),
  dataTableOutput('mytable'),
  plotOutput('scatter')
 )
@@ -16,7 +16,7 @@ server = function(input, output) {
   dataset <- reactive({
     totals[,c('team',input$x,input$y)]
   })
-  p = reactive({ggplot(dataset(),aes_string(x=input$x, y=input$y))+geom_point()})
+  p = reactive({ggplot(dataset(),aes_string(x=input$x, y=input$y,color = input$y))+geom_point(size=5, shape = 16)+scale_color_gradient(low = "#0091ff", high = "#f0650e")})
   output$scatter = renderPlot(p())
 }
 
--- epl_scraper_games.R
+++ epl_scraper_games.R
@@ -262,6 +262,7 @@ combineddb$against_top_6_flag = if_else(combineddb$opp_standing_rank <=6, 1,0)
 combineddb$value_point = combineddb$standing_difference/ combineddb$gameweek_points
 # creating an averages tables
 averages = aggregate(. ~ team, combineddb[,1:16], mean)
+totals = aggregate(. ~ team, combineddb[,1:16], sum)
 
 # exporting data
 # setwd("/Users/admin/Dropbox/dataprojects/epl/exports")
