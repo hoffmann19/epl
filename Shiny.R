bb0925081fde60d50b9e8e1bac0816d0cb5366b7
Charliechaf <cchafin@labelinsight.com>
2017-12-04 22:42:00 -0600

    just copied Dan's code out of Github.

--- Shiny.R
+++ Shiny.R
@@ -1,22 +1,25 @@
 library(shiny)
-install.packages('DT')
+# install.packages('DT')
 library(DT)
 library(ggplot2)
 #teamchoices = sort(unlist(as.list(unique(combineddb$team))))
-columnlist = sort(unlist(as.list(colnames(Averages[,c(2:length(colnames(Averages)))]))))
+columnlist = sort(unlist(as.list(colnames(averages[,c(2:length(colnames(averages)))]))))
 
 ui <- fluidPage(
- selectInput('x','Choose your X axis',choices = columnlist,selected = 'Goals'),
- selectInput('y','Choose your Y axis',choices = columnlist, selected = 'Passes'),
- dataTableOutput('mytable'),
- plotOutput('scatter')
+  selectInput('x','Choose your X axis',choices = columnlist,selected = 'Goals'),
+  selectInput('y','Choose your Y axis',choices = columnlist, selected = 'Passes'),
+  dataTableOutput('mytable'),
+  plotOutput('scatter')
 )
 
 server = function(input, output) {
   dataset <- reactive({
-    totals[,c('team',input$x,input$y)]
+    averages[,c('team',input$x,input$y)]
   })
-  p = reactive({ggplot(dataset(),aes_string(x=input$x, y=input$y,color = input$y))+geom_point(size=5, shape = 16)+scale_color_gradient(low = "#0091ff", high = "#f0650e")})
+  p = reactive({ggplot(dataset(),aes_string(x=input$x, y=input$y,color = input$y))+
+      # geom_point(size=5, shape = 16) +
+      geom_text(aes(label = team)) +
+      scale_color_gradient(low = "#0091ff", high = "#f0650e")})
   output$scatter = renderPlot(p())
 }
 
