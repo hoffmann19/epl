02d9639963b6f0e17b675ac1a1036b79cb8cdbc9
Charliechaf <cchafin@labelinsight.com>
2017-12-05 00:28:50 -0600

    Fixing the ggplot

--- Shiny.R
+++ Shiny.R
@@ -1,7 +1,7 @@
-library(shiny)
+#library(shiny)
 # install.packages('DT')
-library(DT)
-library(ggplot2)
+#library(DT)
+#library(ggplot2)
 #teamchoices = sort(unlist(as.list(unique(combineddb$team))))
 columnlist = sort(unlist(as.list(colnames(averages[,c(2:length(colnames(averages)))]))))
 
@@ -14,12 +14,13 @@ ui <- fluidPage(
 
 server = function(input, output) {
   dataset <- reactive({
-    averages[,c('team',input$x,input$y)]
+    totals[,c('team','points',input$x,input$y)]
   })
-  p = reactive({ggplot(dataset(),aes_string(x=input$x, y=input$y,color = input$y))+
+  p = reactive({ggplot(dataset(),aes_string(x=input$x, y=input$y))+
       # geom_point(size=5, shape = 16) +
-      geom_text(aes(label = team)) +
-      scale_color_gradient(low = "#0091ff", high = "#f0650e")})
+      geom_text(aes(label = team, size = points)) +
+      scale_size(range = c(4, 10)) +
+      scale_color_gradient(high = "#0DCC30", low = "#84170E")})
   output$scatter = renderPlot(p())
 }
 
