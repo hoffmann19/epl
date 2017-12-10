7e087d8217a054a0362acdad3b8e82a9299bd739
Charliechaf <cchafin@labelinsight.com>
2017-12-10 02:02:07 -0600

    Removed the color Palette

--- Shiny.R
+++ Shiny.R
@@ -2,26 +2,39 @@ library(shiny)
 # install.packages('DT')
 library(DT)
 library(ggplot2)
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 #teamchoices = sort(unlist(as.list(unique(combineddb$team))))
-columnlist = sort(unlist(as.list(colnames(averages[,c(2:length(colnames(averages)))]))))
+columnlist = sort(unlist(as.list(colnames(totals[,c(2:length(colnames(totals)))]))))
 
 ui <- fluidPage(
   selectInput('x','Choose your X axis',choices = columnlist,selected = 'Goals'),
   selectInput('y','Choose your Y axis',choices = columnlist, selected = 'Passes'),
+  numericInput('clusters', 'Cluster count', 3, min = 1, max = 9),
   dataTableOutput('mytable'),
   plotOutput('scatter')
+  
 )
 
 server = function(input, output) {
   dataset <- reactive({
-    totals[,c('team','points',input$x,input$y)]
+    totals[,c('team','points',input$x,input$y)] 
+  })
+  kmean <- reactive({
+    totals[,c(input$x,input$y)] 
   })
   p = reactive({ggplot(dataset(),aes_string(x=input$x, y=input$y))+
-      # geom_point(size=5, shape = 16) +
-      geom_text(aes(label = team, size = points)) +
-      scale_size(range = c(4, 10)) +
-      scale_color_gradient(high = "#0DCC30", low = "#84170E")})
+      #geom_point(size=5, shape = 16) +
+      scale_color_hue(l=65, c=100)+
+      geom_text(aes(label = team, size = points, color = factor(clusters()$cluster))) +
+      scale_size(range = c(4, 10))})
   output$scatter = renderPlot(p())
+  
+  clusters <- reactive({
+    kmeans(dataset()[,3:4],input$clusters, 
+           nstart = 20)
+  })
+  
+  
 }
 
 shinyApp(ui = ui, server = server)
