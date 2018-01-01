1c15305ccd77867e90e7b15604a0a0274ff9611f
Charliechaf <cchafin@labelinsight.com>
2017-12-31 18:34:42 -0600

    returns a column that gives an outcome for each match

--- Future Games.r
+++ Future Games.r
@@ -1,4 +1,7 @@
-for(i in 1:1){
+#basic little loop to simulate game outcome
+outcome = data.frame()
+for(i in 1:nrow(fixtures)){
+  print(i)
   temptable = fixtures[i,]
   #temptable = inner_join(temptable,totals, by=c("team"= "team"))
   matchnumber = match(temptable$HOME.TEAM[1], totals$Team.Name)
@@ -7,14 +10,9 @@ for(i in 1:1){
   matchnumber = match(temptable$AWAY.TEAM[1], totals$Team.Name)
   #temptable$awaygoals.allowed = totals$Goals.Allowed[matchnumber]
   temptable$awaymatchscore = (((totals$Goals[matchnumber]-totals$Goals.Allowed[matchnumber])*.10 +totals$points[matchnumber])*.85)
-  temptable$homeprob = (temptable$homematchscore- temptable$awaymatchscore)
+  drawodds = abs(abs(temptable$awaymatchscore - temptable$homematchscore)-100)*.007
+  temptable$outcome = sample(c("home","away","draw"), size=1, replace=TRUE, prob=c((temptable$homematchscore[1]*.01),(temptable$awaymatchscore[1]*.01),drawodds))
+  outcome = rbind(outcome,temptable)
 }
+#write.csv(outcome,'testoutput.csv')
 
-
-set.seed(1)
-sample(c("win","loss","draw"), size=10, replace=TRUE, prob=c(0.2,0.6,0.2))
-
-team =4
-totals$Team.Name[team][1]
-testscore = ((totals$Goals[team]-totals$Goals.Allowed[team])*.6+totals$points[team])
-testscore
